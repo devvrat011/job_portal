80c105c32a18039c551526e18e1de3dfdecf8107
Devvrat Gupta <devvrat.gupta11@gmail.com>
2024-10-06 15:55:23 +0530

    add pre installation

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,11 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from 'path';
 
 dotenv.config({});
+
+const __dirname = path.resolve();
 
 const app = express();
 const corsOptions = {
@@ -24,9 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
-
 const PORT = process.env.PORT || 3000;
-
 
 // api's
 app.use("/api/v1/user", userRoute);
@@ -34,7 +35,11 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 app.listen(PORT,()=>{
     connectDB();
