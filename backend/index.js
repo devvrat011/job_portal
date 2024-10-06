26923bb56e1b58b3246080ec56c71ef50dad563c
Devvrat Gupta <devvrat.gupta11@gmail.com>
2024-10-06 16:54:53 +0530

    ONE LAST COMMIT

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config({});
 
 const app = express();
 const corsOptions = {
-    origin:'http://127.0.0.1:5173',
+    origin:'https://job-portal-54ue.onrender.com/',
     credentials:true
 }
 
