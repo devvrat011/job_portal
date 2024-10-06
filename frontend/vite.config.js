5756ba336ce7565f2717434745fe75f2bc98c95e
Devvrat Gupta <devvrat.gupta11@gmail.com>
2024-10-07 01:04:13 +0530

    thiss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'https://job-portal-seven-rho.vercel.app/',
-        secure:false,
-      },
-    },
-  },
+  // server:{
+  //   proxy:{
+  //     '/api':{
+  //       target:'https://job-portal-seven-rho.vercel.app/',
+  //       secure:false,
+  //     },
+  //   },
+  // },
   plugins: [react()],
   resolve: {
     alias: {
